c172beb83b49032f31f3272ca1837de4303ca1a4
sandro <morpheus@rabbithole.ch>
2020-11-04 15:15:45 +0100

    - cleanup

--- src/engine/navigationevents.js
+++ src/engine/navigationevents.js
@@ -5,65 +5,32 @@
 import {app} from "@/engine/app";
 
 const div = document.querySelector(".scene");
-
 /**
- * Get real element offset with an offsetParent referencing element
- *
- * `getBoundingClientRect` return values is not correct under CSS multi-column
- * layout, so we recursively get `offsetLeft`/`offsetTop` instead.
- * @param {HTMLElement} el - element of interest
- * @param {HTMLElement} stopEl - one of the offsetParent as a reference
- * @return {object} object wth top and left offset
- */
-export function getClientOffset(el, stopEl) {
-  /// tail-called optimize
-  function getOffset(el, stopEl, left = 0, top = 0) {
-    if (el === stopEl) {
-      return {left, top};
-    }
-    return getOffset(
-      el.offsetParent,
-      stopEl,
-      left + el.offsetLeft,
-      top + el.offsetTop,
-    );
-  }
-
-  return getOffset(el, stopEl);
-}
-
-/**
- * Mouse listener which corrects offsets and scrolling positions
+ * Mouse listener which calculates device normalized coordinates [-1, +1]
  * @param target
  * @param {mousemove | click} eventName
- * @param {(x: number, y: number, event: MouseEvent) => void} listener
+ * @param {(x: number, y: number) => void} listener receives normalized Device normalized coordinates [-1, +1]
  */
 function registerMouseListener(target, eventName, listener) {
-  const offset = getClientOffset(target, document.body);
-  function onMouseMove(event) {
-    console.log("offset", document.scrollingElement.scrollTop);
-    const x = event.offsetX / event.target.width * window.devicePixelRatio;
-    const y = -(event.offsetY / event.target.height * window.devicePixelRatio);
+  target.addEventListener(eventName, function (event) {
+    // calculate mouse position in normalized device coordinates
+    // (-1 to +1) for both components
 
-    listener(x, y, event);
-  }
+    const x = (event.offsetX / event.target.width * window.devicePixelRatio) * 2 - 1;
+    const y = -(event.offsetY / event.target.height * window.devicePixelRatio) * 2 + 1;
 
-  target.addEventListener(eventName, onMouseMove, false);
+    listener(x, y);
+  }, false);
 }
 
-registerMouseListener(div, "mousemove", (x, y, event) => {
-  // calculate mouse position in normalized device coordinates
-  // (-1 to +1) for both components
-  app.agentCube.mouseMove.x = x * 2 - 1;
-  app.agentCube.mouseMove.y = y * 2 + 1;
+registerMouseListener(div, "mousemove", (x, y) => {
+  app.agentCube.mouseMove.x = x;
+  app.agentCube.mouseMove.y = y;
 });
 
 registerMouseListener(div, "click", (x, y, event) => {
-  console.log("mouseClick", event);
-  // calculate mouse position in normalized device coordinates
-  // (-1 to +1) for both components
-  app.agentCube.mouseClick.x = x * 2 - 1;
-  app.agentCube.mouseClick.y = y * 2 + 1;
+  app.agentCube.mouseClick.x = x;
+  app.agentCube.mouseClick.y = y;
 });
 
 function onMouseWheel(event) {
