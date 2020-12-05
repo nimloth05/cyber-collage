ebbaf453426807e74a39f0cff3b3175b63f256d9
sandro <morpheus@rabbithole.ch>
2020-12-05 13:41:27 +0100

    - changes from AR

--- src/engine/navigationevents.ts
+++ src/engine/navigationevents.ts
@@ -1,8 +1,25 @@
-//* **************************************************
-//  Mouse, Scroll, and Touch Handlers
-//* **************************************************
-
 import {app} from "@/engine/app";
+
+// ***************************************************
+// Helper Functions
+// ***************************************************
+
+async function promptUserforOrientationPermission() {
+  if (DeviceOrientationEvent && typeof DeviceOrientationEvent.requestPermission === "function") {
+    const permissionState = await DeviceOrientationEvent.requestPermission();
+    if (permissionState === "granted") {
+// Permission granted
+      return true;
+    } else {
+// Permission denied
+      return false;
+    }
+  }
+}
+
+//* **************************************************
+// Mouse, Scroll
+//* **************************************************
 
 // FIXME: Redundant to initializer code
 const div = document.querySelector(".scene")!;
@@ -15,8 +32,8 @@ const div = document.querySelector(".scene")!;
  */
 function registerMouseListener(target: Element, eventName: "mousemove" | "click", listener: (x: number, y: number) => void) {
   target.addEventListener(eventName, function (event) {
-    // calculate mouse position in normalized device coordinates
-    // (-1 to +1) for both components
+// calculate mouse position in normalized device coordinates
+// (-1 to +1) for both components
     const mouseEvent = event as MouseEvent;
     const eventTarget = mouseEvent.target as HTMLCanvasElement;
 
@@ -37,16 +54,57 @@ registerMouseListener(div, "click", (x, y) => {
   app.agentCube.mouseClick.y = y;
 });
 
-function onMouseWheel(event: Event) {
-  const mouseEvent = event as WheelEvent;
-  mouseEvent.preventDefault();
-  if (mouseEvent.shiftKey) {
-    app.agentCube.camera.trackZoom(mouseEvent.deltaX, mouseEvent.deltaY);
-  } else if (mouseEvent.altKey) {
-    app.agentCube.camera.trackSpinn(mouseEvent.deltaX, mouseEvent.deltaY);
+function onMouseWheel(event: any) {
+  event.preventDefault();
+  if (event.shiftKey) {
+    app.agentCube.camera.trackZoom(event.deltaX, event.deltaY);
+  } else if (event.altKey) {
+    app.agentCube.camera.trackSpinn(event.deltaX, event.deltaY);
   } else {
-    app.agentCube.camera.trackPan(mouseEvent.deltaX, mouseEvent.deltaY);
+    app.agentCube.camera.trackPan(event.deltaX, event.deltaY);
   }
 }
 
 div.addEventListener("wheel", onMouseWheel, {passive: false, capture: true});
+
+// ***************************************************
+// Touch Handlers
+// ***************************************************
+
+let ongoingTouches: TouchList;
+let neverTouched = true;
+
+function handleStart(event: any) {
+  if (neverTouched) {
+    promptUserforOrientationPermission().then((enabled) => console.log("orientation is", enabled));
+    neverTouched = false;
+  }
+  event.preventDefault();
+  console.log("touchstart");
+  console.log(event.changedTouches);
+  console.log("start", event.changedTouches[0].radiusX, event.changedTouches[0].radiusX);
+// touch start pretends to be also a mouse click
+  const box = div.getBoundingClientRect();
+  app.agentCube.mouseClick.x = (event.changedTouches[0].clientX - box.left) / event.target.width * window.devicePixelRatio * 2 - 1;
+  app.agentCube.mouseClick.y = -(event.changedTouches[0].clientY - box.top) / event.target.height * window.devicePixelRatio * 2 + 1;
+  console.log("click x: ", app.agentCube.mouseClick.x, " y: ", app.agentCube.mouseClick.y);
+  ongoingTouches = event.changedTouches;
+}
+
+div.addEventListener("touchstart", handleStart, false);
+
+function handleMove(event: any) {
+  event.preventDefault();
+  console.log(event.changedTouches);
+// console.log("moved", event.changedTouches[0].clientY - div.getBoundingClientRect().top);
+  console.log("move", event.changedTouches[0].radiusX, event.changedTouches[0].radiusX);
+// console.log("touch move dx=", event.changedTouches[0].clientX - ongoingTouches[0].clientX, "dy= ", event.changedTouches[0].clientY - ongoingTouches[0].clientY);
+  app.agentCube.camera.trackPan(-event.changedTouches[0].clientX + ongoingTouches[0].clientX, -event.changedTouches[0].clientY + ongoingTouches[0].clientY, 2.0);
+  ongoingTouches = event.changedTouches;
+}
+
+div.addEventListener("touchmove", handleMove, false);
+
+// ***************************************************
+// Orientation
+// ***************************************************
