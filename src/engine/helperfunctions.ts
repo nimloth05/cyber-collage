94994543b73623183329ff5ebca815e120f01f38
Alexander Repenning <62620203+agentcubes@users.noreply.github.com>
2021-01-12 13:55:30 +0100

    limted success mesh/geometry transformation experiements

--- src/engine/helperfunctions.ts
+++ src/engine/helperfunctions.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-labels */
 //* **************************************************
 // Helper Functions
 //* **************************************************
@@ -29,6 +30,63 @@ function depthOfNesting(child: Object3D, parent: Object3D): number {
   return -1;
 }
 
+export function meshGeometry(mesh: Object3D) {
+  let geometry: any = null;
+  mesh.traverse((child: any) => {
+    if (child.geometry != null) {
+      console.log("-- geometry found at level", depthOfNesting(child, mesh), child);
+      if (geometry === null) {
+        geometry = child.geometry;
+      } else {
+        console.error("multiple geometries found");
+      }
+    }
+  });
+  return geometry;
+}
+
+/* JS for console
+function meshGeometry(mesh) {
+  let geometry = null;
+  mesh.traverse(child => {
+    if (child.geometry != null) {
+      // console.log("-- geometry found at level", depthOfNesting(child, mesh), child);
+      if (geometry === null) {
+        geometry = child.geometry;
+      } else {
+        console.error("multiple geometries found");
+      }
+    }
+  });
+  return geometry;
+}
+*/
+
+export function centerAndNormalizeShape(mesh: Object3D) {
+  const geometry = meshGeometry(mesh);
+  const box = geometry.boundingBox;
+  geometry.center();
+  geometry.computeBoundingBox();
+  // most glTF models appear to oriented like a pig laying down on the x axis, looking towards minus x, and feet -y
+  geometry.rotateX(Math.PI * 0.5);
+  // geometry.rotateY(Math.PI * 0.5);
+  // geometry.translate(0, -box.max.y, 0);
+  geometry.translate(0, 0, box.max.z);
+  // geometry.translate(0, -10, 0);
+  mesh.updateMatrixWorld();
+}
+
+export function normalizeGeometry(mesh: Object3D, size = 1.0, name: string) {
+  const box = new Box3();
+  box.setFromObject(mesh);
+  if (name === "Cow_no_tipping") console.log("normalizing cube", box.min.y, box);
+  const scale = size / Math.max(box.max.x - box.min.x, box.max.y - box.min.y);
+  mesh.scale.x = scale;
+  mesh.scale.y = scale;
+  mesh.scale.z = scale;
+  mesh.updateMatrixWorld();
+}
+
 export function centerMeshGeometryOnGround(mesh: Object3D) {
   // Center the geometry and make it flush on ground (z = 0)
   // FIXME: Use better type here
@@ -41,23 +99,14 @@ export function centerMeshGeometryOnGround(mesh: Object3D) {
       child.geometry.center();
       const box = new Box3();
       box.setFromObject(child);
+      // normalizeGeometry(child, 20);
       /// HACK: somehow the mesh returned by glTF is rotated: to translate the z value in the mesh the y value of the geometry needs to be updated
       // child.geometry.translate(-box.min.y, 0, 0);
-      child.geometry.translate(0.0, -box.min.y, 0.0);
+      // child.geometry.translate(0.0, -box.min.y, 0.0);
       // child.geometry.translate(0.0, 0.0, +box.min.z);
     }
   });
   console.log("END mesh processing");
-  mesh.updateMatrixWorld();
-}
-
-export function normalizeGeometry(mesh: Object3D, size = 1.0) {
-  const box = new Box3();
-  box.setFromObject(mesh);
-  const scale = size / Math.max(box.max.x - box.min.x, box.max.y - box.min.y);
-  mesh.scale.x = scale;
-  mesh.scale.y = scale;
-  mesh.scale.z = scale;
   mesh.updateMatrixWorld();
 }
 
